b092dcb575fd5e002c93661502271c5850947777
shAdow-XJY <1290232854@qq.com>
2021-06-08 14:29:43 +0800

    first

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        result = request.form
+
     return render_template('index.html')
+
 
 
 if __name__ == '__main__':
